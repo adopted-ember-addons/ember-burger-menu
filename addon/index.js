e1c1f836c099aeb93299a4567f5943268bfe532e
offirgolan <offirgolan@gmail.com>
2016-11-10 01:50:23 +1300

    Remove comments

--- addon/index.js
+++ addon/index.js
@@ -1,22 +1,3 @@
-// import Ember from 'ember';
 import MenuState from 'ember-burger-menu/-private/menu-state';
-
-// const {
-//   assert,
-//   isEmpty
-// } = Ember;
-
-// const Menus = {};
-//
-// export default function burgerMenu(id = 'main') {
-//   assert('The passed id must be a string', typeof id === 'string');
-//   assert('The passed id cannot be null, undefined, or an empty string', !isEmpty(id));
-//
-//   if (!Menus[id]) {
-//     Menus[id] = MenuState.create({ id });
-//   }
-//
-//   return Menus[id];
-// }
 
 export default MenuState.create();
