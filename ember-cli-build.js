5c8b62cf49cab9d1f5386fc2aab3724253242e90
Josemar Luedke <josemarluedke@gmail.com>
2018-07-30 12:18:14 -0700

    Upgrade bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,11 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    // Add options here
+    'ember-bootstrap': {
+      bootstrapVersion: 4,
+      importBootstrapFont: false,
+      importBootstrapCSS: false
+    }
   });
 
   /*
