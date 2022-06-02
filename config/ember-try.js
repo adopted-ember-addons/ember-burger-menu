6b5007de7ebc94d3939bcee228030832ed6b6afb
Robert Wagner <rwwagner90@gmail.com>
2022-06-02 10:28:31 -0400

    ember-auto-import 2, remove deps

--- config/ember-try.js
+++ config/ember-try.js
@@ -4,6 +4,7 @@ const getChannelURL = require('ember-source-channel-url');
 
 module.exports = async function() {
   return {
+    useYarn: true,
     scenarios: [
       {
         name: 'ember-lts-3.4',
@@ -45,7 +46,7 @@ module.exports = async function() {
           }
         }
       },
-      // The default `.travis.yml` runs this scenario via `npm test`,
+      // The default `.travis.yml` runs this scenario via `yarn test`,
       // not via `ember try`. It's still included here so that running
       // `ember try:each` manually or from a customized CI config will run it
       // along with all the other scenarios.
--- testem.js
+++ testem.js
@@ -1,10 +1,12 @@
-/* global process */
-
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome'],
-  launch_in_dev: ['Chrome'],
+  launch_in_ci: [
+    'Chrome'
+  ],
+  launch_in_dev: [
+    'Chrome'
+  ],
   browser_args: {
     Chrome: {
       ci: [
